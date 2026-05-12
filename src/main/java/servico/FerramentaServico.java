89fb8361b09e277ba64048f52f9ec8d1e4882c7a
Caio Candido <caio2006cunha@gmail.com>
2026-05-12 09:52:33 -0300

    feat: add list method to FerramentaServico

--- src/main/java/servico/FerramentaServico.java
+++ src/main/java/servico/FerramentaServico.java
@@ -1,7 +1,23 @@
 
 package servico;
 
+import java.util.List;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import modelo.Ferramenta;
 
+/**
+ * Interface do serviço SOAP de Ferramenta.
+ */
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface FerramentaServico {
     
+    /**
+     * Retorna a lista de todas as ferramentas cadastradas.
+     */
+    @WebMethod(operationName = "listar")
+    List<Ferramenta> listar();
 }
