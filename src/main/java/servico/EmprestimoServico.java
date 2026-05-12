fd7a69fd584639cdc340f8502ba721cb4cb105bd
Caio Candido <caio2006cunha@gmail.com>
2026-05-12 10:34:16 -0300

    feat: add delete method to EmprestimoServico

--- src/main/java/servico/EmprestimoServico.java
+++ src/main/java/servico/EmprestimoServico.java
@@ -32,5 +32,11 @@ public interface EmprestimoServico {
      */
     @WebMethod(operationName = "alterar")
     boolean alterar(@WebParam(name = "emprestimo") Emprestimo emprestimo);
+    
+    /**
+     * Remove um empréstimo pelo seu id.
+     */
+    @WebMethod(operationName = "deletar")
+    boolean deletar(@WebParam(name = "id") int id);
  
 }
