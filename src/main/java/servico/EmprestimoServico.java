be85f50e1c87588996275ba48694e9e79475022a
Caio Candido <caio2006cunha@gmail.com>
2026-05-12 10:33:34 -0300

    feat: add update method to EmprestimoServico

--- src/main/java/servico/EmprestimoServico.java
+++ src/main/java/servico/EmprestimoServico.java
@@ -26,4 +26,11 @@ public interface EmprestimoServico {
      */
     @WebMethod(operationName = "inserir")
     boolean inserir(@WebParam(name = "emprestimo") Emprestimo emprestimo);
+    
+    /**
+     * Altera um empréstimo existente.
+     */
+    @WebMethod(operationName = "alterar")
+    boolean alterar(@WebParam(name = "emprestimo") Emprestimo emprestimo);
+ 
 }
