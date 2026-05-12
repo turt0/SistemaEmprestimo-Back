e0ae8a7389e5ea7a7a2b4c63a2f444f892730bfb
Caio Candido <caio2006cunha@gmail.com>
2026-05-12 10:30:58 -0300

    feat: add list method to EmprestimoServico

--- src/main/java/servico/EmprestimoServico.java
+++ src/main/java/servico/EmprestimoServico.java
@@ -1,7 +1,23 @@
 
 package servico;
 
+import java.util.List;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import modelo.Emprestimo;
 
-public class EmprestimoServico {
+/**
+ * Interface do serviço SOAP de Empréstimo.
+ */
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface EmprestimoServico {
     
+    /**
+     * Retorna a lista de todos os empréstimos.
+     */
+    @WebMethod(operationName = "listar")
+    List<Emprestimo> listar();
 }
