2ebf4ddb53c29bc3ebeb26a4a72f51583f001181
Caio Candido <caio2006cunha@gmail.com>
2026-05-11 21:19:26 -0300

    add: create interface AmigoServico

---
+++ src/main/java/servico/AmigoServico.java
@@ -0,0 +1,12 @@
+package servico;
+
+import java.util.List;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import modelo.Amigo;
+
+public class AmigoServico {
+    
+}
