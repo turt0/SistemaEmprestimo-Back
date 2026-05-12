5a488eb5fcc7ee897cc6dc6d305fe855651873d8
Caio Candido <caio2006cunha@gmail.com>
2026-05-12 10:19:10 -0300

    feat: add list method FerramentaServicoImpl

--- src/main/java/servico/FerramentaServicoImpl.java
+++ src/main/java/servico/FerramentaServicoImpl.java
@@ -1,7 +1,23 @@
 
 package servico;
 
-
-public class FerramentaServicoImpl {
+import dao.FerramentaDAO;
+import java.util.ArrayList;
+import java.util.List;
+import javax.jws.WebService;
+import modelo.Ferramenta;
+ 
+/**
+ * Implementação do serviço SOAP de Ferramenta.
+ */
+@WebService(endpointInterface = "servico.FerramentaServico")
+public class FerramentaServicoImpl implements FerramentaServico{
     
+    private final FerramentaDAO dao = new FerramentaDAO();
+ 
+    @Override
+    public List<Ferramenta> listar() {
+        System.out.println("FerramentaServico: executando listar()");
+        return new ArrayList<>(dao.getMinhaLista());
+    }
 }
